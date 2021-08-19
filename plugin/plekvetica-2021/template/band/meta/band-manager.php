9a950c557b07128440cb0e8a210693dbf70b8ec7
dans-art <spy15@bluewin.ch>
2021-08-19 20:03:20 +0200

    Update band-manager.php
    - Bugfix for title

--- plugin/plekvetica-2021/template/band/meta/band-manager.php
+++ plugin/plekvetica-2021/template/band/meta/band-manager.php
@@ -3,6 +3,11 @@
 extract(get_defined_vars());
 $band_object = isset($template_args[0]) ? $template_args[0] : null;
 $edit_band_link = $band_object->get_band_link() . '?do=edit_band';
+
+/* @todo: Placeholder. Remove this as soon as the "Änderungen vorschlagen button is working" */
+if (!PlekUserHandler::user_can_edit_band($band_object)){
+  return;
+}
 ?>
 
 <?php PlekTemplateHandler::load_template('text-bar', 'components', __('Band verwalten', 'pleklang')); ?>
@@ -10,9 +15,6 @@ $edit_band_link = $band_object->get_band_link() . '?do=edit_band';
   <div class="band-edit-buttons">
     <?php //Allow everyone to suggest changes to band. ?>
     <?php //PlekTemplateHandler::load_template('button', 'components', '', 'Änderung vorschlagen'); ?>
-
-    <?php //If nobody has claimed the band, show this button ?>
-    <?php //PlekTemplateHandler::load_template('button', 'components', '', 'Diese Band verwalten'); ?>
 
     <?php if (PlekUserHandler::user_can_edit_band($band_object)) : ?>
       <?php PlekTemplateHandler::load_template('button', 'components', $edit_band_link, 'Band bearbeiten'); ?>
